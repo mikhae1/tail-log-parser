6f03d955c25d9a199cfd432c9de2e1a7f18864a2
mmekhanov <mikhail.mekhanov@eurochem.ru>
2016-03-11 17:12:51 +0300

    worked

--- logparse.rb
+++ logparse.rb
@@ -3,93 +3,82 @@
 require 'time'
 
 TAIL_BUF_LENGTH = 2 ** 13 # 8192
-TS_REGEXP = /super\w{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}/
-
-class IO
-  def tail_hours(hours)
-    return [] if hours < 1
-
-    if File.size(self) < TAIL_BUF_LENGTH
-      #TAIL_BUF_LENGTH = File.size(self)
-      return self.readlines.reverse
-    end
-
-    ts_stop = (DateTime.now - (hours/24.0)).to_time
-
-    self.seek(-TAIL_BUF_LENGTH, IO::SEEK_END)
-
-    out = ''
-    line_count = 0
-    read_count = 0
-    begin
-      p size
-      buf = self.read(TAIL_BUF_LENGTH)
-      line_count += buf.count('\n')
-      read_count += buf.length
-      out += buf
-
-      seek_val = 2 * -TAIL_BUF_LENGTH
-
-      if (self.pos < TAIL_BUF_LENGTH)
-        seek_val = -2 * self.pos
-      end
-
-      #p self.pos
-      # if (self.pos == 0)
-      #   p 'End'
-      # end
-
-      self.seek(seek_val, IO::SEEK_CUR)
-
-      ts = buf[TS_REGEXP]
-      if ts != nil
-        cur_time = Time.parse(ts)
-      end
-      # p 'cur_time: ', cur_time, buf
-      # p 'pos: ' + self.pos.to_s
-    end while self.pos != 0 && (cur_time == nil || cur_time >= ts_stop)
-
-    return out.split('\n')[-line_count..-1]
-  end
-end
-
+TS_RE = /\w{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}/
+DB_DELAY_RE = /nd-db:time.*([0-9]+\.[0-9]+)ms/
 
 class LogParser
-  # Regexp to match the timestamps in the apache common log format
-  TIME_REGEXP   = /\w{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}/
-
-  def initialize(log_path)
+  def initialize(log_path, hours)
     raise ArgumentError unless File.exists?(log_path)
 
-    @log          = File.open(log_path)
-    @delta        = 1/24.0 # 1 hour
-    @ending_at    = DateTime.now.to_time
-    @starting_at  = (DateTime.now - @delta).to_time
+    @log = File.open(log_path, 'r')
+    @hours = hours.to_i
+    @buffer_size = TAIL_BUF_LENGTH
+    @stats = {
+      :db_count => 0,
+      :db_avg_delay => 0
+    }
   end
 
-  def emit(&block)
-    @log.tail_hours(1)
-    # @log.each_line do |line|
-    #   next unless timestamp = line.match(TIME_REGEXP)
+  def process
+    delta = @hours/24.0
+    ts_stop = (DateTime.now - delta).to_time
 
-    #   current_time = Time.parse(timestamp[0])
+    # we start at the end
+    @log.seek(0, IO::SEEK_END)
+    offset = @log.pos
+    stop_flag = false
+    while offset > 0 && stop_flag == false
+      if (offset - @buffer_size) < 0
+        to_read = offset
+      else
+        to_read = @buffer_size
+      end
 
-    #   puts current_time
+      @log.seek(offset - to_read)
+      data = @log.read(to_read)
 
-    #   if current_time >= @starting_at && current_time <= @ending_at
-    #     yield line
-    #   end
+      offset -= data.length
 
-    #   if current_time > @ending_at
-    #     return
-    #   end
-    # end
+      last_ts_str = data[TS_RE]
+      if last_ts_str != nil
+        last_ts = Time.parse(last_ts_str)
+      end
+      if last_ts != nil && last_ts < ts_stop
+        stop_flag = true;
+        # find stop position in the current buffer
+        match_arr = data.scan(TS_RE)
+        part_data = ''
+        for i in 0 ... match_arr.size
+          ts = Time.parse(match_arr[i])
+          if (ts > ts_stop)
+            stop_pos = data.index(match_arr[i])
+            part_data = data.slice(stop_pos, data.length)
+            break
+          end
+        end
+        data = part_data
+      end
+
+      self.calc(data)
+    end
+
+    # @log.seek(offset)
+    # return data = @log.read
+    puts 'Results:'
+    puts "db_records_found\t#{@stats[:db_count]}"
+    puts "db_avg_delay\t#{@stats[:db_avg_delay]}"
+  end
+
+  def calc(data)
+    db_delays = data.scan(DB_DELAY_RE).collect{|i| i[0].to_f}
+    @stats[:db_count] += db_delays.length
+    @stats[:db_avg_delay] = (@stats[:db_avg_delay] + db_delays.reduce(:+).to_f / db_delays.size) / 2.0
   end
 
 end
 
 if __FILE__ == $0
   parser = LogParser.new(*ARGV)
+  parser.process()
 
-  parser.emit {|l| puts l}
 end
