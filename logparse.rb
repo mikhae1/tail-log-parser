7c090c37298d60f5fc94feb90dcc6f89150b2dd1
mmekhanov <mikhail.mekhanov@eurochem.ru>
2016-03-15 20:41:40 +0300

    Ready

--- logparse.rb
+++ logparse.rb
@@ -1,35 +1,33 @@
 #!/usr/bin/env ruby
-# FIXME:
-# skip values on the border of buffer
 
 require 'time'
 
 TAIL_BUF_LENGTH = 2 ** 13 # 8192
 TS_RE = /\w{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}/
-DB_DELAY_RE = /nd-db:time.*([0-9]+\.[0-9]+)ms/
+DB_DELAY_RE = /nd-db:time.*([0-9]+\.[0-9]*)ms/
 
 class LogParser
-  def initialize(log_path, hours)
+  def initialize(log_path)
     raise ArgumentError unless File.exists?(log_path)
 
     @log = File.open(log_path, 'r')
-    @hours = hours.to_f
     @buffer_size = TAIL_BUF_LENGTH
     @stats = {
-      :db_count => 0,
-      :db_avg_delay => 0
+      db_count: 0,
+      db_avg_delay: 0
     }
   end
 
-  def process
-    delta = @hours/24.0
+  def process(hours)
+    delta = hours.to_f/24.0
     ts_stop = (DateTime.now - delta).to_time
 
-    # we start from the end
+    # starting from the end
     @log.seek(0, IO::SEEK_END)
     offset = @log.pos
     stop_flag = false
-    while offset > 0 && stop_flag == false
+    data_tail = nil
+    while offset > 0 && !stop_flag
       if (offset - @buffer_size) < 0
         to_read = offset
       else
@@ -38,16 +36,21 @@ class LogParser
 
       @log.seek(offset - to_read)
       data = @log.read(to_read)
-
       offset -= data.length
 
-      last_ts_str = data[TS_RE]
-      if last_ts_str != nil
+      data = data + data_tail if data_tail
+
+      last_ts_str = data[TS_RE] # first ts
+      data_tail = nil
+      if last_ts_str
+        data_tail = data[0 ... data.index(last_ts_str)]
         last_ts = Time.parse(last_ts_str)
       end
-      if last_ts != nil && last_ts < ts_stop
+
+      # the end of the tail
+      if last_ts && last_ts < ts_stop
+        # find the stop position
         stop_flag = true;
-        # find stop position in the current buffer
         match_arr = data.scan(TS_RE)
         part_data = ''
         for i in 0 ... match_arr.size
@@ -64,6 +67,7 @@ class LogParser
       self.calc(data)
     end
 
+    # result output
     # @log.seek(offset)
     # return data = @log.read
     puts 'Results:'
@@ -82,6 +86,6 @@ class LogParser
 end
 
 if __FILE__ == $0
-  parser = LogParser.new(*ARGV)
-  parser.process()
+  parser = LogParser.new(ARGV[0])
+  parser.process(ARGV[1])
 end
